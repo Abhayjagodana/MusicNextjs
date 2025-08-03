02d3b1f390f92d8e30fb87e37aa19d083a4208c6
Abhi <jagodanaabhay@gmail.com>
2025-08-03 15:53:44 +0530

    nextjs

--- src/Componet/3Dcard.tsx
+++ src/Componet/3Dcard.tsx
@@ -3,6 +3,8 @@
 import React from "react";
 import { CardBody, CardContainer, CardItem } from "./ui/3d-card";
 import coursedata from '../Data/Music_course.json';
+import Image from "next/image";
+
 function Card() {
     return (
         <div>
@@ -13,7 +15,7 @@ function Card() {
             </div>
             <div className="flex flex-wrap gap-4 m-4">
                 {coursedata.courses.map((course) => (
-                    <CardContainer className="inter-var ">
+                    <CardContainer className="inter-var " key={course.id}>
                         <CardBody className="bg-gray-50 relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded-xl p-6 border  ">
                             <CardItem
                                 translateZ="50"
@@ -29,12 +31,12 @@ function Card() {
                                 {course.description}
                             </CardItem>
                             <CardItem translateZ="100" className="w-full mt-4">
-                                <img
+                                <Image
                                     src={course.image}
-                                    height="1000"
-                                    width="1000"
-                                    className="h-60 w-full object-cover rounded-xl group-hover/card:shadow-xl"
+                                    height={240}
+                                    width={480}
                                     alt={course.title}
+                                    className="h-60 w-full object-cover rounded-xl group-hover/card:shadow-xl"
                                 />
                             </CardItem>
                             <div className="flex justify-between items-center mt-20">
